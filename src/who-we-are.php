2838cd1947415b210c20141d21131aed50588ffa
Roshan Gupta <roshan.gupta@occamsadvisory.com>
2025-12-31 01:04:12 +0530

    who we are banner responsive and add

--- src/who-we-are.php
+++ src/who-we-are.php
@@ -4,16 +4,32 @@ $metakeyword = "";
 include "header.php";
 ?>
 
-<section class="lg:px-24 sm:px-7 px-5 py-10">
-    <div class="mx-auto max-w-7xl">
-        <div class="grid grid-cols-12 gap-6 items-start">
-            <div class="col-span-12 lg:col-span-10">
-                <div id="stack" class="relative h-65 overflow-visible"></div>
-            </div>
-        </div>
+<!-- Hero Section   -->
+<section class="hero-background lg:bg-center bg-right px-5">
+    <div class="container mx-auto py-45">
+        <h1 class="lg:text-[52px] sm:text-4xl text-2xl font-bold text-white text-center">
+            Principled Engineering for <br class="sm:flex hidden"> Responsible Intelligence
+        </h1>
+
+        <p class='lg:text-[22px] md:text-lg text-base text-white font-normal
+            text-center pt-3'>
+            Every system we build carries clarity, evidence, and accountability at its core.
+        </p>
     </div>
 </section>
 
 <?php
 include 'footer.php';
 ?>
+
+<style>
+    .hero-background {
+        background-image: url('./assets/images/who-we-are-page-banner.svg');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+
+    .spacing {
+        padding: 50px 0px;
+    }
+</style>
